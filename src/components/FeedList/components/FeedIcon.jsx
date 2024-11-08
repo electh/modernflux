08d36b8bb294f3d000880f8b1fd86b01a73d765c
lisicheng <noteperson@gmail.com>
2024-11-09 06:49:10 +0800

    添加收藏功能

--- src/components/FeedList/components/FeedIcon.jsx
+++ src/components/FeedList/components/FeedIcon.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-import { Image } from '@nextui-org/react';
+import { useState } from "react";
+import { Image } from "@nextui-org/react";
+
 const FeedIcon = ({ url }) => {
   const [error, setError] = useState(false);
   const [isBlurry, setIsBlurry] = useState(false);
-  
+
   // 从URL中提取域名
   const getDomain = (url) => {
     try {
-      const domain = new URL(url).hostname;
-      return domain;
+      return new URL(url).hostname;
     } catch {
-      return '';
+      return "";
     }
   };
 
@@ -37,19 +37,19 @@ const FeedIcon = ({ url }) => {
   // 如果URL无效、图片加载失败或图片模糊，显示默认图标
   if (!url || error || isBlurry) {
     return (
-        <svg 
-          className="w-5 h-5 text-gray-500" 
-          fill="none" 
-          stroke="currentColor" 
-          viewBox="0 0 24 24"
-        >
-          <path 
-            strokeLinecap="round" 
-            strokeLinejoin="round" 
-            strokeWidth={2} 
-            d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z" 
-          />
-        </svg>
+      <svg
+        className="w-5 h-5 text-gray-500"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z"
+        />
+      </svg>
     );
   }
 
