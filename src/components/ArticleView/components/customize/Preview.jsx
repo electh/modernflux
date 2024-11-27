84eb1926fd243417a4e6539ed110a458a465f83b
lisicheng <noteperson@gmail.com>
2024-11-27 14:08:54 +0800

    样式优化

--- src/components/ArticleView/components/customize/Preview.jsx
+++ src/components/ArticleView/components/customize/Preview.jsx
@@ -39,7 +39,7 @@ export default function Preview() {
           textAlign: alignJustify ? "justify" : "left",
         }}
       >
-        &ldquo;白日依山尽&quot;写远景，写山，写的是登楼望见的景色，&ldquo;黄河入海流&quot;写近景，写水写得景象壮观，气势磅礴，诗人既高度形象又高度概括地把进入广大视野的万里河山，收入短短十个字中。
+        &ldquo;白日依山尽&rdquo;写远景，写山，写的是登楼望见的景色，&ldquo;黄河入海流&rdquo;写近景，写水写得景象壮观，气势磅礴，诗人既高度形象又高度概括地把进入广大视野的万里河山，收入短短十个字中。
       </div>
     </div>
   );
