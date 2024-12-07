39ccb88a3edbcfc15cdc47a621b3d62dc28a319e
lisicheng <noteperson@gmail.com>
2024-12-07 09:25:46 +0800

    集成三方媒体播放器（WIP）

--- src/hooks/useIsIOS.js
+++ src/hooks/useIsIOS.js
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from "react";
 
 export function useIsIOS() {
   const [isIOS, setIsIOS] = useState(false);
 
   useEffect(() => {
     const userAgent = window.navigator.userAgent.toLowerCase();
+    console.log(userAgent);
     const isIOSDevice = /iphone|ipad|ipod/.test(userAgent);
     setIsIOS(isIOSDevice);
   }, []);
